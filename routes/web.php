ea8ac5582fc4dd4421753e269c3a4b7cf23e1e2d
Hameed <hameedawkumcs@gmail.com>
2025-05-06 15:03:00 +0500

    Routing

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,20 @@ Route::get("user/{name}/{age}",function($name, $age){
     return "I am $name and my age is $age";
 
 });
+
+
+
+Route::match(['get','post'],'contact/submit',function(){
+    return "I am contact submit page";
+});
+
+
+Route::get("user/{name?}",function($name = null){
+    return "my name is $name";
+
+});
+
+Route::redirect('contact','about',301);
+
+Route::view('about','about');
+Route::view('student','student',['name' => 'Ali khan','age' => 22]);
